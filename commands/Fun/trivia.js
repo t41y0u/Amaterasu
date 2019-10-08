e7fbac3c9d0a9859aa28439d8f5981d79a208e6b
Kaguya-chan <vuthaiduong0607@gmail.com>
2019-10-08 20:19:37 +0700

    Added categories for a!trivia

--- commands/Fun/trivia.js
+++ commands/Fun/trivia.js
@@ -4,13 +4,40 @@ const { stripIndents } = require("common-tags");
 const snekfetch = require("snekfetch");
 const h = new (require("html-entities").AllHtmlEntities)();
 
+const categoryChoices = {
+    "General Knowledge": "9",
+    "Entertainment: Books": "10",
+    "Entertainment: Film": "11",
+    "Entertainment: Music": "12",
+    "Entertainment: Musicals & Theatres": "13",
+    "Entertainment: Television": "14",
+    "Entertainment: Video Games": "15",
+    "Entertainment: Board Games": "16",
+    "Science & Nature": "17",
+    "Science: Computers": "18",
+    "Science: Mathematics": "19",
+    "Mythology": "20",
+    "Sports": "21",
+    "Geography": "22",
+    "History": "23",
+    "Politics": "24",
+    "Art": "25",
+    "Celebrities": "26",
+    "Animals": "27",
+    "Vehicles": "28",
+    "Entertainment: Comics": "29",
+    "Science: Gadgets": "30",
+    "Entertainment: Japanese Anime & Manga": "31",
+    "Entertainment: Cartoon & Animations":" 32"
+};
+
 class Trivia extends Command {
     constructor(client) {
         super(client, {
             name: "trivia",
             description: "Puts your general knowledge to the test.",
             category: "Fun",
-            usage: "trivia [difficulty]",
+            usage: "trivia [difficulty] [category]",
             aliases: ["randomtrivia", "randomq", "testme", "quiz"]
         });
     }
@@ -18,9 +45,53 @@ class Trivia extends Command {
     async run(message, args, level) {
         try {
             const levels = ["easy", "medium", "hard"];
-            const difficulty = args[0] || "medium";
-            if (!levels.includes(difficulty.toLowerCase())) return this.client.embed("commonError", message, "Invalid difficulty specified. Please choose from one of **easy**, **medium** or **hard**.");
-            const { body } = await snekfetch.get(`https://opentdb.com/api.php?amount=50&difficulty=${difficulty.toLowerCase()}&type=multiple`);
+            const categories = [
+                "General Knowledge", 
+                "Entertainment: Books", 
+                "Entertainment: Film", 
+                "Entertainment: Music",
+                "Entertainment: Musicals & Theatres",
+                "Entertainment: Television",
+                "Entertainment: Video Games",
+                "Entertainment: Board Games",
+                "Science & Nature",
+                "Science: Computers",
+                "Science: Mathematics",
+                "Mythology",
+                "Sports",
+                "Geography",
+                "History",
+                "Politics",
+                "Art",
+                "Celebrities",
+                "Animals",
+                "Vehicles",
+                "Entertainment: Comics",
+                "Science: Gadgets",
+                "Entertainment: Japanese Anime & Manga",
+                "Entertainment: Cartoon & Animations"
+            ];
+            let difficulty = "medium";
+            let category = "General Knowledge";
+            if (args[0]) {
+                if (args[0].toLowerCase() === "categories") {
+                    const embed = new RichEmbed()
+                        .setColor(0x00FFFF)
+                        .setAuthor("Trivia", "https://vgy.me/9UDUk0.png")
+                        .setDescription(`**Available categories are:** \n${categories.join("\n")}`)
+                        .setTimestamp()
+                        .setFooter(`Requested by ${message.author.tag}`, message.author.displayAvatarURL)
+                    return message.channel.send({embed});
+                }
+                else if (!levels.includes(args[0].toLowerCase())) category = args.join(" ");
+                else {
+                    difficulty = args[0].toLowerCase();
+                    if (args[1]) category = args.splice(1).join(" ");
+                }
+            }
+            if (!levels.includes(difficulty)) return this.client.embed("commonError", message, "Invalid difficulty specified. Please choose from one of **easy**, **medium** or **hard**.");
+            if (!categories.includes(category)) return this.client.embed("commonError", message, `Invalid category specified. Please choose from one of **${categories.join("**, **")}**.`);
+            const { body } = await snekfetch.get(`https://opentdb.com/api.php?amount=10&difficulty=${difficulty}&category=${categoryChoices[category]}&type=multiple`);
             const quiz = body.results.random();
             const choices = quiz.incorrect_answers.map(ans => h.decode(ans));
             choices.push(h.decode(quiz.correct_answer));
